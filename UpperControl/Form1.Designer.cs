4d04cd1613e5286e498d37317df2b57a933da80a
Gaea <xuweiiam@gmail.com>
2015-04-20 19:07:52 +0800

    Add a icon for this application.

--- UpperControl/Form1.Designer.cs
+++ UpperControl/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(hypocrisyForm));
             this.hypocrisyComboBox1 = new System.Windows.Forms.ComboBox();
             this.hypocrisyComboBox2 = new System.Windows.Forms.ComboBox();
             this.hypocrisyButton1 = new System.Windows.Forms.Button();
@@ -672,6 +673,7 @@
             this.Controls.Add(this.hypocrisyComboBox1);
             this.Controls.Add(this.hypocrisyHead);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "hypocrisyForm";
             this.Text = "UpperControl";
